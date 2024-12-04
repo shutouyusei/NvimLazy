274ae85ecb5a22e32b32e7e8384f7c78c722ca72
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2024-12-04 13:10:21 +0900

    file分割

---
+++ lua/plugins/nvim_cmp.lua
@@ -0,0 +1,18 @@
+return {
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+    end,
+  },
+}
