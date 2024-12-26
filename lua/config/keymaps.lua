bda18024b01f24fed0aa6b72d5ecf0e499228fe0
首藤優誠 <111882261+shutouyusei@users.noreply.github.com>
2024-12-26 10:38:18 +0900

    [add]powershell

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,8 @@
 vim.keymap.set("i", "jj", "<ESC>")
 -- ターミナルノーマルモードへの変更
 vim.keymap.set("t", "jj", "<C-\\><C-n>")
+-- terminal
+vim.keymap.set("n", "<leader>t", function()
+  Snacks.terminal("powershell", { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<leader>t", "<cmd>close<CR>", { desc = "Terminal (Root Dir)" })
