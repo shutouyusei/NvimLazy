495d0b52883169a156e78a6bddd225b1b2005f17
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2026-08-24 16:57:00 +0900

    review-explain: add @return annotations to display.show

--- lua/review_explain/display.lua
+++ lua/review_explain/display.lua
@@ -3,6 +3,8 @@ local M = {}
 ---Show `lines` in a floating window styled like LSP hover.
 ---@param lines string[]
 ---@param opts table|nil extra options merged into vim.lsp.util.open_floating_preview's opts
+---@return integer bufnr
+---@return integer winnr
 function M.show(lines, opts)
 	opts = opts or {}
 	vim.lsp.util.open_floating_preview(lines, "markdown", vim.tbl_extend("force", {
